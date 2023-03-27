fe13f634d1a632268b11a2d270b956490165f4c9
LucyKanyua <lucy.munyange@gmail.com>
2023-03-27 06:47:51 -0700

    add _printf.c

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _printf - produces output according to the format
+ * @format: list of type of arguments passed to the function
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar((char)va_arg(args, int));
+					break;
+				case 's':
+					count += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(*format);
+					break;
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	return (count);
+}
