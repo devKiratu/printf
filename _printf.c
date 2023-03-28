af28985e5da0694a870e45f35acea73a2d07b3d5
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-28 12:36:34 +0300

    add custom format specifier %b

--- _printf.c
+++ _printf.c
@@ -20,32 +20,49 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-				case 'c':
-					count += _putchar((char)va_arg(args, int));
-					break;
-				case 's':
-					count += _puts(va_arg(args, char *));
-					break;
-				case 'd':
-				case 'i':
-					count += print_number(va_arg(args, int));
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					count += _putchar('%');
-					count += _putchar(*format);
-					break;
-			}
+			count += format_output(format, args);
 		}
 		else
 		{
 			count += _putchar(*format);
 		}
 		format++;
+	}
+	return (count);
+}
+
+/**
+ * format_output - prints to stdout using the format specified
+ * @format: format specifier
+ * @args: argument list
+ * Return: number of characters printed
+ */
+int format_output(const char *format, va_list args)
+{
+	int count = 0;
+
+	switch (*format)
+	{
+		case 'c':
+			count += _putchar((char)va_arg(args, int));
+			break;
+		case 's':
+			count += _puts(va_arg(args, char *));
+			break;
+		case 'd':
+		case 'i':
+			count += print_number(va_arg(args, int));
+			break;
+		case 'b':
+			count += print_binary(va_arg(args, int));
+			break;
+		case '%':
+			count += _putchar('%');
+			break;
+		default:
+			count += _putchar('%');
+			count += _putchar(*format);
+			break;
 	}
 	return (count);
 }
