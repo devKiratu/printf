36063bdd146b4f148ee6cff420850159fbe4686e
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-28 12:35:50 +0300

    register prototypes for functions print_binary, convert_to_bin, and format_output

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int _puts(char *s);
 int _printf(const char *format, ...);
 int print_number(int n);
+int print_binary(unsigned int i);
+void convert_to_bin(unsigned int i);
+int format_output(const char *format, va_list args);
 
 #endif
