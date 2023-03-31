5bbd3a04c5217fc8963b90a6f83ed23b0179e698
LucyKanyua <lucy.munyange@gmail.com>
2023-03-31 11:03:09 -0700

    add some checks for conversion specifier o

--- print_hex.c
+++ print_hex.c
@@ -37,3 +37,27 @@ int print_lower_hex(va_list args)
 	convert_to_hex(i, 'x');
 	return (count);
 }
+/**
+ * convert_to_hex - converts to hexadecimal
+ * @i: parameter
+ * @fmt: parameter
+ */
+void convert_to_hex(unsigned int i, char fmt)
+{
+	if (i == 0)
+		return;
+	int rd = i % 16;
+
+	i /= 16;
+	convert_to_hex(i, fmt);
+
+	if (rd > 9)
+	{
+		rd -= 10;
+		_putchar((fmt == 'x' ? 'a' : 'A') + rd);
+	}
+	else
+	{
+		_putchar('0' + rd);
+	}
+}
