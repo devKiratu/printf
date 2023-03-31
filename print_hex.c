f09ec8b3901215b7fb8e933cc20d6f7e71f941e0
LucyKanyua <lucy.munyange@gmail.com>
2023-03-31 10:24:43 -0700

    add some checks for conversion specifier x

--- print_hex.c
+++ print_hex.c
@@ -18,3 +18,22 @@ int print_upper_hex(va_list args)
 	convert_to_hex(i, 'X');
 	return (count);
 }
+/**
+ * print_lower_hex - prints lower hexadecimals
+ * @args: - list of arguments
+ * Return: lower hexadecimals
+ */
+int print_lower_hex(va_list args)
+{
+	unsigned int i = va_arg(args, unsigned int);
+	int count = 0;
+	unsigned int num = i;
+
+	while (num > o)
+	{
+		num /= 16;
+		count++;
+	}
+	convert_to_hex(i, 'x');
+	return (count);
+}
